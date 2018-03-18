e94082205bbff062fa5f823b495476930e39f265
Adam Perry <adam.n.perry@gmail.com>
2018-03-17 18:51:20 -0700

    Run benchmarks inside a cpuset shield for better predictability.

--- run.sh
+++ run.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-taskset -c 1-7 cargo bench
+
+set +xeo pipefail
+
+cleanup_shield() {
+  # if we're exiting we want to free up all of our resources
+  cset shield --reset
+}
+
+trap cleanup_shield EXIT SIGTERM
+
+# sets up a cpuset "shield" by creating two cpusets and moving all running processes into the smaller one
+cset shield --cpu=1-7 --kthread=on
+
+# just print some info
+cset shield
+
+# run our benchmarks under the cpuset shield
+cset sh whoami
+cset sh cargo +stable bench
