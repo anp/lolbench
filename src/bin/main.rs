2769a19a257dc85205409720703e026f359f9fa9
Adam Perry <adam.n.perry@gmail.com>
2018-03-22 23:22:11 -0700

    Add basic structure for two-tier bench runner.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,51 @@
 extern crate clap;
+extern crate lolbench;
+#[macro_use]
+extern crate structopt;
+
+use std::process::{Command, Stdio};
+
+use structopt::StructOpt;
+
+use lolbench::cpu_shield::RenameThisCommandWrapper;
+
+#[derive(StructOpt, Debug)]
+struct Options {
+    #[structopt(short = "t", long = "toolchain")]
+    toolchain: String,
+    #[structopt(short = "c", long = "cpus")]
+    cpu_pattern: Option<String>,
+    #[cfg(target_os = "linux")]
+    #[structopt(short = "k", long = "move-kthreads")]
+    move_kernel_threads: bool,
+}
 
 fn main() {
-  println!("hello world");
+    let opt = Options::from_args();
+
+    // TODO check if rustup knows about the toolchain
+    // TODO if rustup doesn't, install it, set uninstall flag
+    println!(
+        "Building benchmark runner with {} toolchain...",
+        opt.toolchain
+    );
+
+    Command::new("cargo")
+        // use rustup shortcut for invoking toolchain
+        .arg(format!("+{}", opt.toolchain))
+        .arg("build")
+        .arg("--release")
+        .arg("--bin")
+        .arg("run_benches")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect("failed to spawn benchmark builder");
+
+    println!("Running benchmarks...");
+    let exit = RenameThisCommandWrapper::new("target/release/run_benches")
+        .status()
+        .expect("failed to run benchmarks");
+
+    println!("{:?}", exit);
 }
---
+++ src/cpu_shield.rs
@@ -0,0 +1,94 @@
+use std::ffi::OsStr;
+use std::io;
+use std::ops::{Deref, DerefMut};
+use std::process::{Child, Command, ExitStatus, Output};
+
+pub struct RenameThisCommandWrapper {
+    cmd: Command,
+    cpu_mask: Option<String>,
+    #[cfg_attr(not(target_os = "linux"), allow(dead_code))]
+    kthread_on: bool,
+}
+
+impl RenameThisCommandWrapper {
+    pub fn new<S: AsRef<OsStr>>(cmd: S) -> Self {
+        // cset sh ${CMD}
+        // cset shield --reset
+        Self {
+            cmd: Command::new(cmd),
+            cpu_mask: None,
+            kthread_on: false,
+        }
+    }
+
+    pub fn cpu_mask<S: AsRef<str>>(&mut self, mask: S) -> &mut Self {
+        self.cpu_mask = Some(String::from(mask.as_ref()));
+        self
+    }
+
+    #[cfg(target_os = "linux")]
+    fn setup_shield(&mut self) -> io::Result<()> {
+        if let Some(ref mask) = self.cpu_mask {
+            // cset shield --cpu=${CPU_MASK} --kthread=${on|off}
+
+            let mut shield_create = Command::new("cset");
+            shield_create.arg(format!("--cpu={}", mask));
+
+            if self.kthread_on {
+                shield_create.arg("--kthread=on");
+            }
+            unimplemented!("setup shield not yet implemented");
+        }
+        Ok(())
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    fn setup_shield(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+
+    #[cfg(target_os = "linux")]
+    fn teardown_shield(&mut self) -> io::Result<()> {
+        unimplemented!("teardown shield not yet implemented");
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    fn teardown_shield(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+
+    pub fn spawn(&mut self) -> io::Result<Child> {
+        unimplemented!("only status method supported");
+    }
+
+    pub fn output(&mut self) -> io::Result<Output> {
+        unimplemented!("only status method supported");
+    }
+
+    pub fn status(&mut self) -> io::Result<ExitStatus> {
+        self.setup_shield()?;
+
+        let status = self.cmd.status();
+
+        // TODO teardown shield
+        match self.teardown_shield() {
+            Ok(_) => (),
+            Err(why) => (), // TODO log warning
+        }
+
+        status
+    }
+}
+
+impl Deref for RenameThisCommandWrapper {
+    type Target = Command;
+    fn deref(&self) -> &Self::Target {
+        &self.cmd
+    }
+}
+
+impl DerefMut for RenameThisCommandWrapper {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.cmd
+    }
+}
