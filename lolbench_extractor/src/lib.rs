c35f8871dc4382196c16745e53cf8ac7ef1f2a60
Adam Perry <adam.n.perry@gmail.com>
2018-08-02 21:58:35 -0700

    move bench generation & parsing into separate crate, add tests

--- lolbench_extractor/src/lib.rs
+++ lolbench_extractor/src/lib.rs
@@ -1,24 +1,15 @@
-#![recursion_limit = "256"]
 #[macro_use]
 extern crate proc_macro_hack;
-#[macro_use]
-extern crate quote;
 
-#[allow(unused_imports)]
-#[macro_use]
 extern crate failure;
-extern crate proc_macro2;
+extern crate marky_mark;
 extern crate slug;
-extern crate syn;
-extern crate toml;
 
 use std::env;
-use std::fs::read_to_string;
 use std::path::Path;
 
-use proc_macro2::Span;
+use marky_mark::Benchmark;
 use slug::slugify;
-use syn::{Ident as SynIdent, Path as SynPath};
 
 type Result<T> = std::result::Result<T, failure::Error>;
 
@@ -36,59 +27,46 @@ fn lolbench_entrypoint(bench_path: &str) -> Result<String> {
         bench_path.trim()
     ));
 
-    let fn_name: SynPath = syn::parse_str(bench_path)?;
-
-    let crate_name = env::var("CARGO_PKG_NAME")?;
+    let crate_name = ::std::env::var("CARGO_PKG_NAME")?;
     let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
     let bins_dir = Path::new(&manifest_dir).join("src").join("bin");
 
-    let crate_name_ident = SynIdent::new(crate_name.trim(), Span::call_site());
-
-    let file_contents = quote! {
-        extern crate #crate_name_ident;
-        extern crate lolbench_support;
-
-        use lolbench_support::{criterion_from_env, init_logging};
-
-        fn main() {
-            init_logging();
-            let mut crit = criterion_from_env();
-            #crate_name_ident::#fn_name(&mut crit);
-        }
-
-        #[test]
-        fn run_bench() {
-            use std::default::Default;
-            use std::time::Duration;
-            use lolbench_support::Criterion;
-            init_logging();
-            let mut crit = Criterion::default();
-
-            crit = crit.sample_size(2);
-            crit = crit.warm_up_time(Duration::from_micros(1));
-            crit = crit.measurement_time(Duration::from_micros(1));
-            crit = crit.nresamples(1);
-
-            #crate_name_ident::#fn_name(&mut crit);
-        }
-    };
-
     let full_path = bins_dir.join(&format!("{}.rs", slugify(bench_path.to_string())));
-    let file_contents = file_contents.to_string();
 
-    // first lets check if we would change anything on disk,
-    // bc if not we don't need to write anything. this isn't important
-    // for perf, but it makes cargo-watch work much better
-    match read_to_string(&full_path) {
-        Ok(ref current) if current == &file_contents => return returned,
-        _ => (),
-    }
+    Benchmark::new(&crate_name, bench_path).write(&full_path)?;
 
-    if let Some(parent) = full_path.parent() {
-        ::std::fs::create_dir_all(parent)?;
-    }
-
-    // TODO(anp): guarantee that rustfmt is available somehow and run it on the file
-    ::std::fs::write(&full_path, file_contents.as_bytes())?;
     returned
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::read_to_string;
+    use std::path::Path;
+    use std::process::Command;
+
+    use marky_mark::Benchmark;
+
+    #[test]
+    fn test_inflate() {
+        let inflate_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("benches")
+            .join("inflate_0_3_4");
+
+        let output = Command::new("cargo")
+            .arg("test")
+            .current_dir(&inflate_dir)
+            .output()
+            .unwrap();
+
+        assert!(
+            output.status.success(),
+            "failed to run inflate's tests: {}",
+            String::from_utf8_lossy(&output.stdout),
+        );
+
+        let decode_path = inflate_dir.join("src").join("bin").join("decode.rs");
+        let decode_contents = read_to_string(&decode_path).unwrap();
+        Benchmark::parse(&decode_contents).unwrap();
+    }
 }
---
+++ marky_mark/src/lib.rs
@@ -0,0 +1,224 @@
+#![recursion_limit = "256"]
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate quote;
+#[macro_use]
+extern crate serde_derive;
+
+extern crate digest;
+extern crate generic_array;
+extern crate proc_macro2;
+extern crate serde;
+extern crate serde_json;
+extern crate sha2;
+extern crate syn;
+extern crate toml;
+
+#[cfg(test)]
+extern crate tempfile;
+
+use std::fs::{create_dir_all, read_to_string, write};
+use std::path::Path;
+
+use proc_macro2::Span;
+use syn::{Ident as SynIdent, Path as SynPath};
+
+type Result<T> = ::std::result::Result<T, failure::Error>;
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Benchmark {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    runner: Option<String>,
+    name: String,
+    #[serde(rename = "crate")]
+    crate_name: String,
+    checksum: Option<Checksum>,
+}
+
+impl Benchmark {
+    pub fn new(crate_name: &str, name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            crate_name: crate_name.to_string(),
+            checksum: None,
+            runner: None,
+        }
+    }
+
+    pub fn set_runner(&mut self, runner: &str) {
+        self.runner = Some(runner.to_owned());
+    }
+
+    fn source(&self) -> String {
+        let name_syn: SynPath = syn::parse_str(&self.name).unwrap();
+        let crate_name_syn = SynIdent::new(&self.crate_name, Span::call_site());
+
+        let source = quote! {
+            extern crate #crate_name_syn;
+            extern crate lolbench_support;
+
+            use lolbench_support::{criterion_from_env, init_logging};
+
+            fn main() {
+                init_logging();
+                let mut crit = criterion_from_env();
+                #crate_name_syn::#name_syn(&mut crit);
+            }
+
+            #[test]
+            fn run_bench() {
+                use std::default::Default;
+                use std::time::Duration;
+                use lolbench_support::Criterion;
+                init_logging();
+                let mut crit = Criterion::default();
+
+                crit = crit.sample_size(2);
+                crit = crit.warm_up_time(Duration::from_micros(1));
+                crit = crit.measurement_time(Duration::from_micros(1));
+                crit = crit.nresamples(1);
+
+                #crate_name_syn::#name_syn(&mut crit);
+            }
+        };
+
+        source.to_string()
+
+        // TODO(anp): guarantee that rustfmt is available somehow and run it on the file
+    }
+
+    pub fn rendered(&mut self) -> String {
+        let source = self.source();
+        self.checksum = Some(Checksum::new(&source));
+        format!("//{}\n{}", serde_json::to_string(&self).unwrap(), source)
+    }
+
+    pub fn parse(s: &str) -> Result<(Self, String)> {
+        let mut lines = s.lines();
+
+        let first_line = match lines.next() {
+            Some(l) => l.trim_left_matches("//"),
+            None => bail!("missing first line"),
+        };
+
+        let remaining = lines.fold(String::new(), |remaining, line| remaining + line);
+
+        Ok((serde_json::from_str(first_line)?, remaining))
+    }
+
+    pub fn write(&mut self, full_path: &Path) -> Result<bool> {
+        let mut file_contents = self.rendered();
+
+        let mut need_to_write = true;
+
+        // if there's an existing file for this bench's path, we need to know about two questions
+        //
+        // 1. is there persistent config that was written before which we need to preserve?
+        // 2. can we skip writing altogether to limit disk thrash?
+        if let Ok(existing_contents) = read_to_string(&full_path) {
+            // for now, the only persistent config is what runner, if any, has been configured.
+            // however, we don't want to preserve the last runner config if we're *currently in the
+            // process of setting it*
+            if self.runner.is_none() {
+                // we don't care about errors here at all
+                if let Ok((existing, _)) = Self::parse(&existing_contents) {
+                    if let Some(r) = existing.runner {
+                        self.runner = Some(r);
+                    }
+                }
+            }
+
+            // need to re-render the contents after potentially mutating
+            file_contents = self.rendered();
+
+            need_to_write = file_contents != existing_contents;
+        }
+
+        if need_to_write {
+            create_dir_all(full_path.parent().unwrap())?;
+            write(&full_path, file_contents.as_bytes())?;
+        }
+
+        Ok(need_to_write)
+    }
+}
+
+use digest::FixedOutput;
+use generic_array::GenericArray;
+use sha2::{Digest, Sha256};
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(tag = "method", content = "value")]
+pub enum Checksum {
+    #[serde(rename = "sha256-generic-array")]
+    Sha256(GenericArray<u8, <Sha256 as FixedOutput>::OutputSize>),
+}
+
+impl Checksum {
+    pub fn new(s: &str) -> Self {
+        Checksum::Sha256(Sha256::digest_str(s))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn roundtrips() {
+        let mut header = Benchmark::new("test_crate", "test_bench");
+        let rendered = header.rendered();
+
+        let (mut parsed, remaining) = Benchmark::parse(&rendered).unwrap();
+        assert_eq!(header, parsed);
+        assert_eq!(remaining, header.source());
+
+        let parsed_rendered = parsed.rendered();
+        assert_eq!(rendered, parsed_rendered);
+    }
+
+    #[test]
+    fn write() {
+        let mut header = Benchmark::new("test_crate", "test_bench");
+        let rendered = header.rendered();
+
+        let tmpfile = tempfile::NamedTempFile::new().unwrap();
+        let bench_path = tmpfile.path();
+        header.write(&bench_path).unwrap();
+
+        let written = read_to_string(&bench_path).unwrap();
+
+        assert_eq!(rendered, written);
+    }
+
+    #[test]
+    fn preserve_runner() {
+        let runner = "they-call-me-tim";
+
+        let mut header = Benchmark::new("test_crate", "test_bench");
+        let mut without_runner = header.clone();
+        header.set_runner(runner);
+
+        let tmpfile = tempfile::NamedTempFile::new().unwrap();
+        let bench_path = tmpfile.path();
+        header.write(&bench_path).unwrap();
+
+        let written = read_to_string(&bench_path).unwrap();
+        let (written_header, _) = Benchmark::parse(&written).unwrap();
+
+        assert_eq!(
+            written_header.runner, header.runner,
+            "runner should be preserved in writing"
+        );
+
+        without_runner.write(&bench_path).unwrap();
+        let written = read_to_string(&bench_path).unwrap();
+        let (written_header, _) = Benchmark::parse(&written).unwrap();
+
+        let written_runner = written_header
+            .runner
+            .expect("runner should have been preserved");
+        assert_eq!(written_runner, runner);
+    }
+}
