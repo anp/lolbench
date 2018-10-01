5aea2f1255fc567707ed1c3eb01372c3aa7239f4
Geoffroy Couprie <geo.couprie@gmail.com>
2018-10-01 12:27:23 +0200

    update to nom 4.0.0

--- benches/nom_4_0_0_beta1/src/bin/arithmetic-arithmetic.rs
+++
@@ -1,1 +0,0 @@
-extern crate nom_4_0_0_beta1 ; extern crate lolbench_support ; use lolbench_support :: { criterion_from_env , init_logging } ; fn main ( ) { init_logging ( ) ; let mut crit = criterion_from_env ( ) ; nom_4_0_0_beta1 :: arithmetic :: arithmetic ( & mut crit ) ; }
--- benches/nom_4_0_0_beta1/tests/arithmetic-arithmetic.rs
+++
@@ -1,1 +0,0 @@
-extern crate lolbench ; # [ test ] fn end_to_end ( ) { lolbench :: end_to_end_test ( "nom_4_0_0_beta1" , "arithmetic::arithmetic" , ) ; }
