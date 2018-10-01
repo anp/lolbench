7359cfd5be241db9b5b817cfe52a3b335936483a
Josh Holmer <jholmer.in@gmail.com>
2018-10-01 09:54:35 -0400

    Add benchmarks for csv crate

---
+++ benches/csv_1_0_2/src/bin/count-pop-serialize-owned-str.rs
@@ -0,0 +1,1 @@
+extern crate csv_1_0_2 ; extern crate lolbench_support ; use lolbench_support :: { criterion_from_env , init_logging } ; fn main ( ) { init_logging ( ) ; let mut crit = criterion_from_env ( ) ; csv_1_0_2 :: count_pop_serialize_owned_str ( & mut crit ) ; }
---
+++ benches/csv_1_0_2/tests/count-nfl-serialize-owned-str.rs
@@ -0,0 +1,1 @@
+extern crate lolbench ; # [ test ] fn end_to_end ( ) { lolbench :: end_to_end_test ( "csv_1_0_2" , "count_nfl_serialize_owned_str" , ) ; }
