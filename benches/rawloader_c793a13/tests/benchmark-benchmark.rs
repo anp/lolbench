1102d8c290d0a91e918f168ee2b1e56ebb412fd5
jhwgh1968 <jhwgh1968@protonmail.com>
2021-04-08 20:50:13 -0500

    Remove extra test files

--- benches/rawloader_c793a13/tests/benchmark-benchmark.rs
+++
@@ -1,3 +0,0 @@
-extern crate lolbench ; # [ test ] fn end_to_end (  ) {
-lolbench :: end_to_end_test ( "rawloader_c793a13" , "benchmark::benchmark" , )
-; }
--- benches/rawloader_c793a13/tests/decode-decode.rs
+++
@@ -1,2 +0,0 @@
-extern crate lolbench ; # [ test ] fn end_to_end (  ) {
-lolbench :: end_to_end_test ( "rawloader_c793a13" , "decode::decode" , ) ; }
